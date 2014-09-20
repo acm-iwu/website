b84372c3140b2f3098878cd72a6154f8982cc203
Cong Vu <cvu1@acm-server.iwu.edu>
2014-09-20 16:12:10 -0500

    update about page

--- about.php
+++ about.php
@@ -5,19 +5,49 @@
 
   require 'template/header.php';
 ?>
- 	
- <b> What we do: </b>	
- 	We carry out our missions through conferences, publications, educational program, public awareness activities, and special interest groups. ACM unites the world's computing educators, researchers and professionals to inspire dialogue, share resources and address the field's challenges. ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking. </br>
 
-<div id="container" style="width:505px">
 
-<div id="header" style="background-color:green;">
-<h1 style="margin-bottom:0;"> About us and our mission</h1></div> 
 
-<div id="content" style="background-color:#EEEEEE;height:260px;width:505px;float:left;">
- We are the Association of Computing Machinery's chapter of Illinois Wesleyan University. ACM is an academic organization focused on Computer Science. ACM unites the world's computing educators, researchers and professionals from all sectors of computing sciences and their applications, from the design and construction of computers to the development of appropriate programming theory and languages and the utilization of computers in scientific investigation, industrial control, management data processing, and the humanities. We inspire dialogues, share resources and address the field's challenges. We also strengthen the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking. </div>
 
+
+
+
+
+<body>	
+
+<div class="row">
+  <div class="col-md-6"> <h1> Description </h1> </br> We are the Association of Computing Machinery's chapter of Illinois Wesleyan University. We carry out our missions through conferences, publications, educational program, public awareness activities, and special interest groups. ACM unites the world's computing educators, researchers and professionals to inspire dialogue, share resources and address the field's challenges. ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking.</div>
+  <div class="col-md-6"> <h1> About us and Mission </h1> </br> ACM is an academic organization focused on Computer Science. ACM unites the world's computing educators, researchers and professionals from all sectors of computing sciences and their applications, from the design and construction of computers to the development of appropriate programming theory and languages and the utilization of computers in scientific investigation, industrial control, management data processing, and the humanities. We inspire dialogues, share resources and address the field's challenges. We also strengthen the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking.</div>
 </div>
+ 
+ 
+ </br>
+ <h1> Officer </h1>
+ 
+ <div class="row">
+  <div class="col-md-3"><img src="pictures/alan.png" alt="Alan Russian - The President" class="img-circle">
+  </br>
+  <h4> Alan Russian </h4>
+  <h4> President </h4>
+  <h4> arussian@iwu.edu </h4>
+  </div>
+  
+  <div class="col-md-3"><img src="..." alt="..." class="img-circle"> </div>
+  
+  <div class="col-md-3"><img src="..." alt="..." class="img-circle"> </div>
+  
+  <div class="col-md-3"><img src="pictures/jeremy.jpg" alt="Jeremy Albert - Chief of Staff" class="img-circle">
+  </br>
+  <h4> Jeremy Albert
+  <h4> Chief of Staff </h4>
+  <h4> jalbert@iwu.edu </h4>
+  </div>
+  
+</div>
+ 
+ 
+ 
+ 
  
  
 <?php
