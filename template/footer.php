a35381206f2d94b8ed2ec7e28e558449c8139671
Josiah Zan <hzan@acm-server.iwu.edu>
2014-09-29 21:18:24 -0500

    Fix bootstrap.min.js file reference.

--- template/footer.php
+++ template/footer.php
@@ -3,7 +3,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="templates/js/bootstrap.min.js"></script>
+    <script src="template/js/bootstrap.min.js"></script>
     </div>
   </body>
 </html>
