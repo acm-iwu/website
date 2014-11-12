5bb14c709e492472e273abff219f40c21cd8521f
ttattini <tantattini@gmail.com>
2014-11-12 15:01:37 -0600

    Quick Fix
    Grammar

--- about.php
+++ about.php
@@ -7,17 +7,11 @@
 ?>
 
 
-
-
-
-
-
-
 <body>	
 
 <div class="row">
   <div class="col-md-6"> <h1> Description </h1> </br> We are the Association of Computing Machinery's chapter of Illinois Wesleyan University. We carry out our missions through conferences, publications, educational program, public awareness activities, and special interest groups. ACM unites the world's computing educators, researchers and professionals to inspire dialogue, share resources and address the field's challenges. ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking.</div>
-  <div class="col-md-6"> <h1> About us and Mission </h1> </br> ACM is an academic organization focused on Computer Science. ACM unites the world's computing educators, researchers and professionals from all sectors of computing sciences and their applications, from the design and construction of computers to the development of appropriate programming theory and languages and the utilization of computers in scientific investigation, industrial control, management data processing, and the humanities. We inspire dialogues, share resources and address the field's challenges. We also strengthen the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking.</div>
+  <div class="col-md-6"> <h1> About us and Mission </h1> </br> ACM is an academic organization focused on Computer Science. ACM unites the world's computing educators, researchers and professionals from all sectors of computing sciences and their applications, from the design and construction of computers to the development of appropriate programming theory and languages and the utilization of computers in scientific investigation, industrial control, management data processing, and the humanities. We inspire dialogues, share resources and address the field's challenges. We strive to strengthen the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence. We support the professional growth of our members by providing opportunities for life-long learning, career development, and professional networking.</div>
 </div>
  
  
