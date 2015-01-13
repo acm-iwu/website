b54962e3d6e1fd3d306a21cf49fb71bc9e2bd471
Tanner Tattini <tantattini@gmail.com>
2015-01-13 12:41:36 +0530

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -4,6 +4,11 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="template/js/bootstrap.min.js"></script>
+      <script src="js/bootstrap-hover.js"></script>
+   
+    <script>
+    $('.dropdown-toggle').dropdownHover(options);
+    </script>
     </div>
   </body>
 </html>
