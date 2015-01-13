6a68c6f2f2584d64f36628bacf840b56cf86e89e
Tanner Tattini <tantattini@gmail.com>
2015-01-13 12:53:08 +0530

    Update send.php

--- send.php
+++ send.php
@@ -4,11 +4,14 @@
 		isset($_POST['subject'])&& isset($_POST['Message'])){
 			if(!empty($_POST['user']) && !empty($_POST['email'])&& 
 		!empty($_POST['subject'])&& !empty($_POST['Message'])){
+			
+			
 			$user = $_POST['user'];
 			$email = $_POST['email'];
 			$subject = $_POST['subject'];
 			$mess = $_POST['Message'];
-			$to = "ihteshamhussain1994@gmail.com";
+			//$to = "t.u.chaudhari@gmail.com";
+			$to = "ttattini@iwu.edu";
 			$subject_text = $subject;
 			$header = "From: '$user' <'$email'> ";
 			$body = $mess;
