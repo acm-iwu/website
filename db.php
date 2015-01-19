d1b6bbad24efba8aac3bcde7f2ebd695b405329b
Tanner Tattini <tantattini@gmail.com>
2015-01-19 22:53:03 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 	mysql_connect('localhost', 'root' ,'') or die("error connecting to the db");
-	mysql_select_db('freelancer') or die("cannot select the db");
+	mysql_select_db('acm') or die("cannot select the db");
 
 ?>
