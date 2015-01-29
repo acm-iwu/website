b5c6420d20ef79bc044a1be064a65bf5eb3b9d8d
Tanner Tattini <tantattini@gmail.com>
2015-01-30 01:10:31 +0530

    Update events.php

--- events.php
+++ events.php
@@ -5,9 +5,18 @@
 
   require 'template/header.php';
 ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Events</h1>
+            <p>Below are on-going IWU ACM Events.</p>
+            <br />
 
 <iframe src="https://www.google.com/calendar/htmlembed?src=%23contacts%40group.v.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="1100" height="600" frameborder="0" scrolling="no"></iframe>
 
+		</div>
+      </div>
+</div>        	
 <?php
   require 'template/footer.php';
 ?>
