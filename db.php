fce11f11176e3c35b509996dbb7c8fbc25ed5b28
ACM <acm@acm-server.iwu.edu>
2015-02-08 14:59:28 -0600

    Move SQL credentials to gitignored file and add sample of said file.

--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
-	mysql_connect('localhost', 'root' ,'') or die("error connecting to the db");
-	mysql_select_db('acm') or die("cannot select the db");
+  require_once 'config.php';
+
+	mysql_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD) or die("error connecting to the db");
+	mysql_select_db(SQL_DATABASE) or die("cannot select the db");
 
 ?>
