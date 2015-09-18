6e7ef6fdbf01e56f818824160cab168f8313a17e
Minh Nguyen <mnminh2209@gmail.com>
2015-09-18 14:46:03 -0500

    Changes

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 define('SQL_HOST', 'localhost');
 define('SQL_USERNAME', 'root');
-define('SQL_PASSWORD', '');
+define('SQL_PASSWORD', 'abc');
 define('SQL_DATABASE', 'website');
 
-	$conn = mysql_connect('localhost', 'root', '') or die ("error connecting to the db");
+	$conn = mysql_connect('localhost', 'root', 'abc') or die ("error connecting to the db");
 	mysql_select_db('website', $conn) or die("cannot select the db");
 
 ?>
