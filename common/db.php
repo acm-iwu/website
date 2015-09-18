ea525d57819b787af17856de12e642b0a6028659
Minh Nguyen <mnminh2209@gmail.com>
2015-09-18 14:24:23 -0500

    Changes

--- common/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once 'config.php';
-	
-	$conn = mysql_connect('localhost', 'root', '') or die ("error connecting to the db");
-	mysql_select_db('website', $conn) or die("cannot select the db");
-
-?>
